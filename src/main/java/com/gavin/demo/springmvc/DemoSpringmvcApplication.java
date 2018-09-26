a859017446f8b0af7f904cd1f3c94fb642a881e8
tony <tony@tonyde-MacBook-Pro.local>
2018-09-26 17:50:22 +0800

    project init

---
+++ src/main/java/com/gavin/demo/springmvc/DemoSpringmvcApplication.java
@@ -0,0 +1,12 @@
+package com.gavin.demo.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringmvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringmvcApplication.class, args);
+    }
+}
---
+++ src/test/java/com/gavin/demo/springmvc/DemoSpringmvcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gavin.demo.springmvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoSpringmvcApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
